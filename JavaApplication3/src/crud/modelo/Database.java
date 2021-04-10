2701472500b865b0798e2cc9a9863cf6dc7dc868
Erick Estrada <erick.e.senado@gmail.com>
2021-04-10 18:22:51 -0500

    Created class Database

--- JavaApplication3/src/crud/modelo/Database.java
+++ JavaApplication3/src/crud/modelo/Database.java
@@ -1,6 +1,34 @@
 package crud.modelo;
 
 public class Database {
+    String url;
+    String usr;
+    String pwd;
+    String driver;
     
-    
+    public Database () {
+    this.url = "jdbc:mysql://localhost:3306/estudiantes";
+    this.usr = "root";
+    this.pwd = "";
+    this.driver = "com.mysql.jdbc.Driver";
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsr() {
+        return usr;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public String getDriver() {
+        return driver;
+    }
+   
 }
+
+
